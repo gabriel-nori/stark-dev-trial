f2786e2c763af830dd0c331fc970b7f1c4f45846
gabriel.nori <gabrielnori@gabrielnori.tech>
2024-10-04 15:51:37 -0300

    Feat: Create test for person

--- app/models/person.py
+++ app/models/person.py
@@ -44,6 +44,19 @@ class Person:
                                                                                     
         return '%s%s%s.%s%s%s.%s%s%s-%s%s' % tuple(cpf)
 
+def validate_cpf(cpf: str):
+    cpf = ''.join(filter(str.isdigit, cpf))
+    ''' Expects a numeric-only CPF string. '''
+    if len(cpf) != 11:
+        return False    
+    
+    if cpf in [s * 11 for s in [str(n) for n in range(10)]]:
+        return False
+    
+    calc = [i for i in range(1, 10)]
+    d1= (sum([int(a)*b for a,b in zip(cpf[:-2], calc)]) % 11) % 10
+    d2= (sum([int(a)*b for a,b in zip(reversed(cpf[:-2]), calc)]) % 11) % 10
+    return str(d1) == cpf[-2] and str(d2) == cpf[-1]
 
 first_names = [
     "Amelia",
--- app/tests/test_person.py
+++ app/tests/test_person.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime, timedelta
+from app.models.person import validate_cpf
 from unittest import TestCase, main
 from app.models import Person
 
@@ -13,6 +14,12 @@ class PersonTest:
         """
 
         assert self.person.birth - timedelta(days=18*365) >= 0
+
+    def test_cpf_validator(self):
+        assert validate_cpf("302.208.800-00") == True
+        assert validate_cpf("077.000.600-02") == True
+        assert validate_cpf("027.000.450-02") == False
+        assert validate_cpf("450-02") == False
     
     def test_cpf(self):
-        a=1
+        assert validate_cpf(self.person.cpf) == True
