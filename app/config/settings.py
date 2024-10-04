d9c98293b34c84cbdc5a6b09db58d86b6d8a12e2
gabriel.nori <gabrielnori@gabrielnori.tech>
2024-10-04 01:05:13 -0300

    Feat: Update example.env and settings module

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import base64
 import os
 
 env_file_path = os.getcwd() + "/.env"
@@ -9,6 +10,6 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "info")
 
 API_ROOT = os.getenv("API_ROOT", "localhost/")
 
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+PRIVATE_KEY = base64.b64decode(os.getenv("PRIVATE_KEY", "").encode("ascii")).decode("ascii")
 
 PROJECT_ID = os.getenv("PROJECT_ID")
